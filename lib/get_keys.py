78f29513651453bd4dea154778a69e1ac24150dc
Trevaun Turner <trevaun.turner@outlook.com>
2020-05-10 08:21:52 -0400

    Fix tab spacing in lib

--- lib/get_keys.py
+++ lib/get_keys.py
@@ -5,12 +5,12 @@ import time
 
 keyList = ["\b"]
 for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
-    keyList.append(char)
+  keyList.append(char)
 
 def key_check():
-    keys = []
-    for key in keyList:
-        if wapi.GetAsyncKeyState(ord(key)):
-            keys.append(key)
-    return keys
+  keys = []
+  for key in keyList:
+    if wapi.GetAsyncKeyState(ord(key)):
+      keys.append(key)
+  return keys
  
